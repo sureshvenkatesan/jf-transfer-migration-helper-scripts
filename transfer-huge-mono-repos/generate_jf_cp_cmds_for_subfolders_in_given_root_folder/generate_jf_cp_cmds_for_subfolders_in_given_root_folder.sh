4f215d1b44e5c9922982ea5a675462c9e8587f9b
Suresh Venkatesan <sureshv@jfrog.com>
2023-09-10 15:28:47 -0700

    AU-0 - Added helper scripts to copy huge repo to generic type repo

---
+++ transfer-huge-mono-repos/generate_jf_cp_cmds_for_subfolders_in_given_root_folder/generate_jf_cp_cmds_for_subfolders_in_given_root_folder.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Check if at least the first four required parameters are provided
+if [ $# -lt 4 ]; then
+    echo "Usage: $0 <source-artifactory> <source-repo> <target-repo> <target-artifactory> [root-folder]"
+    exit 1
+fi
+
+# Assign the input parameters to variables
+source_artifactory="$1"
+source_repo="$2"
+
+target_artifactory="$3"
+target_repo="$4"
+
+
+# Check if the fifth parameter (root-folder) is provided
+if [ $# -ge 5 ]; then
+    root_folder="$5"
+else
+    root_folder=""
+fi
+
+# Run the jf rt curl command and capture the output into a variable
+if [ -z "$root_folder" ]; then
+    #echo jf rt curl  -k -XGET "/api/storage/$source_repo?list&deep=1&depth=1&listFolders=1" --server-id $source_artifactory
+    output=$(jf rt curl  -k -XGET "/api/storage/$source_repo?list&deep=1&depth=1&listFolders=1" --server-id $source_artifactory)
+else
+    #echo jf rt curl  -k -XGET "/api/storage/$source_repo/$root_folder?list&deep=1&depth=1&listFolders=1" --server-id $source_artifactory
+    output=$(jf rt curl  -k -XGET "/api/storage/$source_repo/$root_folder?list&deep=1&depth=1&listFolders=1" --server-id $source_artifactory)
+fi
+
+# Parse the JSON output using jq and get the "uri" values for folders
+folders=$(echo "$output" | jq -r '.files[] | select(.folder) | .uri')
+
+# Split folders into an array
+IFS=$'\n' read -rd '' -a folders_array <<< "$folders"
+
+# Loop through the folders and generate the jf rt cp commands
+for folder in "${folders_array[@]}"; do
+    echo "jf rt cp $source_repo$folder/ $target_repo/ --flat=false --threads=8 --dry-run=false --server-id $target_artifactory"
+done
